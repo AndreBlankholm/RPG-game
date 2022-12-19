009625525ae1aa30b310e6b6e07dcf49b52f7707
AndreBlankholm <ablankholm@gmail.com>
2022-12-19 17:47:33 -0600

    created a potion Constructor

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,17 @@
+const Potion = require('../lib/Potion.js');
+
+test('Creates a health potion Object', () => {
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
+
+// if the Potion constructor is not called with any arguments/ create a new postion with a random type and value//
+test('creates a random potion object', () => {
+    const potion = new Potion();
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+});
--- __tests__/random.tests.js
+++
@@ -1,1 +0,0 @@
-checkIfEqual = require('../lib/')
