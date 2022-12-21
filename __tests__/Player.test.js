101346f00770b51c0d22f8347935ad5b7610adef
AndreBlankholm <ablankholm@gmail.com>
2022-12-21 15:53:40 -0600

    implemented mock tests / wrote test to check for coreect usage of object methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -6,7 +6,7 @@ jest.mock('../lib/Potion');
 
 // console.log the mock potion
 console.log(new Potion());
-
+console.log(new Player());
 
 test('creates a new player object', () => {
     const player = new Player('Dave');
@@ -20,3 +20,21 @@ test('creates a new player object', () => {
       );
 });
 
+test('get players stats as an object', () => {
+  const player = new Player('Dave');
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
+});
